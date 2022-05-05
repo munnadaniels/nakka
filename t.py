477cfb2b23a8979c3fdc5248f12e9bc28d9fcd44
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-05-05 11:45:38 +0530

    Update t.py

--- t.py
+++ t.py
@@ -11,7 +11,7 @@ args = arguments.parse_args()
 
 
 def trackname():
-     os.system('ffmpeg -i -map 0:v -map 0:a -map 0:s? -metadata title="@TROOPORIGINALS" -metadata:s:v title="TroopOriginals" -metadata:s:a title="TroopOriginals" -metadata:s:s title="TroopOriginals" -codec copy test.mkv'%link)
+     os.system('ffmpeg -i {link} -map 0:v -map 0:a -map 0:s? -metadata title="@TROOPORIGINALS" -metadata:s:v title="TroopOriginals" -metadata:s:a title="TroopOriginals" -metadata:s:s title="TroopOriginals" -codec copy {filename}')
      output = f"{filename}"
      subprocess.run(['rclone', output,'Rose:'])
 
